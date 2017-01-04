1c35ff001209a9db92ca14f724c12be702edbcd1
wanghaiwen <1192328490@qq.com>
2017-01-04 15:22:46 +0800

    提交item

--- zhihu/items.py
+++ zhihu/items.py
@@ -15,16 +15,14 @@ class ZhihuItem(scrapy.Item):
     followees = scrapy.Field()  # 用户粉丝
     followers = scrapy.Field()  # 用户关注的人
     introduce = scrapy.Field()  # 简介
-    #ellipsis = scrapy.Field()  # 用户详细介绍
-    #location = scrapy.Field()  # 住址
-    #major = scrapy.Field()  # 主修
+    
     head_image = scrapy.Field()  # 头像url
-    #views = scrapy.Field()  # 浏览次数
+    
     ask = scrapy.Field()  # 提问
     answer = scrapy.Field()  # 回答
-    #articles = scrapy.Field()  # 文章
+   
     collected = scrapy.Field()  # 收藏
-    #public_editor = scrapy.Field()  # 公共编辑
+   
     main_page = scrapy.Field() #主页url
     _id = scrapy.Field()
     image_urls = scrapy.Field()
